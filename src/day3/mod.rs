09e81e666b8cc5af6ab6407cfa723be92f40f5dd
Paul Watson <paul.watson@pdwtech.com>
2018-04-06 22:21:34 +0100

    Day3 part1 Tidy up

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,3 +1,68 @@
 pub mod part1;
 #[cfg(test)]
 pub mod tests;
+
+#[derive(Debug, Clone)]
+struct Location {
+    x : i32,
+    y : i32
+}
+
+#[derive(Debug)]
+struct LocStat {
+    location : Location,
+    direction : Direction
+}
+
+#[derive(Debug, Clone)]
+enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
+}
+
+
+impl LocStat {
+
+    fn turn_and_advance(&self) -> LocStat {
+        let new_direction = self.new_direction();
+        LocStat {
+            location: self.location.clone(),
+            direction: new_direction
+        }.advance()
+    }
+
+    fn advance(&self) -> LocStat {
+        let this = self;
+
+        let new_location = match self.direction {
+            Direction::NORTH => Location { x: this.location.x, y: this.location.y + 1 },
+            Direction::SOUTH => Location { x: this.location.x, y: this.location.y - 1 },
+            Direction::EAST  => Location { x: this.location.x + 1, y: this.location.y },
+            Direction::WEST  => Location { x: this.location.x - 1, y: this.location.y },
+        };
+
+        let new_locstat = LocStat {
+            location: new_location,
+            direction: self.direction.clone()
+        };
+//        println!("advancing to {:?}", l);
+
+        new_locstat
+    }
+
+    fn new_direction(&self) -> Direction {
+        match self.direction {
+            Direction::NORTH => Direction::WEST,
+            Direction::WEST => Direction::SOUTH,
+            Direction::SOUTH => Direction::EAST,
+            Direction::EAST => Direction::NORTH
+        }
+    }
+
+    fn distance(&self) -> u32 {
+        (self.location.x.abs() + self.location.y.abs()) as u32
+    }
+}
+
--- src/day3/part1.rs
+++ src/day3/part1.rs
@@ -1,66 +1,6 @@
-#[derive(Debug, Clone)]
-struct Location {
-    x : i32,
-    y : i32
-}
-
-#[derive(Debug)]
-struct LocStat {
-    location : Location,
-    direction : Direction
-}
-
-#[derive(Debug, Clone)]
-enum Direction {
-    NORTH,
-    SOUTH,
-    EAST,
-    WEST
-}
-
-
-impl LocStat {
-    
-    fn turn_and_advance(&self) -> LocStat {
-        let new_direction = self.new_direction();
-        LocStat {
-            location: self.location.clone(),
-            direction: new_direction
-        }.advance()
-    }
-    
-    fn advance(&self) -> LocStat {
-        let this = self;
-        
-        let new_location = match self.direction {
-            Direction::NORTH => Location { x: this.location.x, y: this.location.y + 1 },
-            Direction::SOUTH => Location { x: this.location.x, y: this.location.y - 1 },
-            Direction::EAST  => Location { x: this.location.x + 1, y: this.location.y },
-            Direction::WEST  => Location { x: this.location.x - 1, y: this.location.y },
-        };
-        
-        let l = LocStat { 
-            location: new_location,
-            direction: self.direction.clone()
-        };
-//        println!("advancing to {:?}", l);
-        
-        l
-    }
-    
-    fn new_direction(&self) -> Direction {
-        match self.direction {
-            Direction::NORTH => Direction::WEST,
-            Direction::WEST => Direction::SOUTH,
-            Direction::SOUTH => Direction::EAST,
-            Direction::EAST => Direction::NORTH
-        }
-    }
-    
-    fn distance(&self) -> u32 {
-        (self.location.x.abs() + self.location.y.abs()) as u32
-    }
-}
+use day3::LocStat;
+use day3::Location;
+use day3::Direction;
 
 pub fn calc_corner(num : u32) -> u32 {
     let ceil_square = (num as f64).sqrt().ceil() as u32;
@@ -77,11 +17,9 @@ pub fn ring(num: u32) -> u32 {
     (((calc_corner(num) as f64).sqrt() as u32) + 1) / 2
 }
 
-
 fn width(num: u32) -> u32 {
     ring(num)* 2 -1
 }
-
 
 fn calc_all_corners(num : u32) -> Vec<u32> {
     let br = calc_corner(num);
@@ -89,22 +27,26 @@ fn calc_all_corners(num : u32) -> Vec<u32> {
     vec!(br, br-(2*side), br-(3*side), br-side)
 }
 
+fn corner_for_ring(ring: u32) -> u32 {
+    let t = ring as i32 * 2 -1;
+    (t * t) as u32
+}
+
 fn should_turn(num: u32) -> bool {
     let three_corners = &calc_all_corners(num)[1..4];
-    let previous_ring = ring(num) -1;
-    let t = previous_ring as i32 * 2 -1;
-    let next_ring = (t * t + 1 ) as u32; 
+    let previous_ring = ring(num) - 1;
+    
     match &num {
         1 | 2 => true,
         x if three_corners.contains(&x) => true,
-        y if *y == next_ring => true,
+        y if *y == corner_for_ring(previous_ring) + 1 => true,
         _ => false
     }
 }
 
 pub fn solve(num: u32) -> u32 {
     let mut locstat = LocStat { 
-        location: Location{ x : 0, y: 0},
+        location: Location { x : 0, y: 0 },
         direction: Direction::SOUTH
     };
 
@@ -117,6 +59,8 @@ pub fn solve(num: u32) -> u32 {
     
     println!("distance : {}   {:?}", locstat.distance(), locstat);
     locstat.distance()
+}
+
 //    37 36  35  34  33  32 31
 //    38 17  16  15  14  13 30
 //    39 18   5   4   3  12 29
@@ -130,5 +74,3 @@ pub fn solve(num: u32) -> u32 {
 //    2 1 0 1 2
 //    3 2 1 2 3
 //    4 3 2 3 4
-    
-}
