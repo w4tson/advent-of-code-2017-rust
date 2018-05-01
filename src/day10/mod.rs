9fe44bac8d63da0621174cac9ae7449f74601f37
Paul Watson <paul.watson@pdwtech.com>
2018-05-01 22:25:22 +0100

    Day10 part1 refactor to KnotHash trait

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -8,7 +8,7 @@ pub fn knot_hash(input_size: i32, input_lengths: &Vec<usize>) -> i32 {
     let mut skip = 0;
     
     for length in input_lengths {
-        swap_length(&mut input, pos, *length);
+        input.rev_circular_sub_slice(pos, *length);
         pos = ((pos + length + skip) as i32 % input_size) as usize;
         skip += 1;
     }
@@ -16,14 +16,18 @@ pub fn knot_hash(input_size: i32, input_lengths: &Vec<usize>) -> i32 {
     input[0] * input[1]
 }
 
-fn swap_length(input: &mut Vec<i32>, pos : usize, length: usize) {
-    let ref mut sub = input.iter().cloned().cycle().skip(pos).take(length).collect::<Vec<i32>>();
-    sub.reverse();
-    for i in 0..length {
-        let circular_index = (i + pos) % input.len();
-        input[circular_index..circular_index+1].swap_with_slice(&mut sub[i..i+1]);
-    }
-//    println!(" Pos: [{}]  Skip: {}  Length: {}  Reversed {:?}", pos, skip, length, sub);
-//    println!("{:?}", input);
+trait KnotHash {
+    fn rev_circular_sub_slice(&mut self, pos: usize, length: usize);
 }
 
+impl KnotHash for [i32] {
+    
+    fn rev_circular_sub_slice(&mut self, pos: usize, length: usize) {
+        let ref mut sub = self.iter().cloned().cycle().skip(pos).take(length).collect::<Vec<i32>>();
+        sub.reverse();
+        for i in 0..length {
+            let circular_index = (i + pos) % self.len();
+            self[circular_index..circular_index+1].swap_with_slice(&mut sub[i..i+1]);
+        }
+    }
+}
