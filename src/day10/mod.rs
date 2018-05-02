9e95d5473c5d3ab8dee1a7695962178274500a72
Paul Watson <paul.watson@pdwtech.com>
2018-05-02 21:40:31 +0100

    Day10 part2 refactor

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -15,9 +15,7 @@ pub fn knot_hash2<T : Into<String>>(input_str: T) -> String {
     let mut skip : usize = 0;
     
     for _ in 0..64 {
-        let (new_pos, new_skip) = knot_hash_round(&mut input, 256, &input_lengths, pos, skip);
-        pos = new_pos;
-        skip = new_skip;
+        knot_hash_round(&mut input, 256, &input_lengths, &mut pos, &mut skip);
     }
     
     to_hex(&compute_dense_hash(&input))
@@ -26,20 +24,17 @@ pub fn knot_hash2<T : Into<String>>(input_str: T) -> String {
 
 pub fn knot_hash(input_size: i32, input_lengths: &Vec<usize>) -> i32 {
     let mut input: Vec<i32> = (0..input_size).collect();
-    let (pos, skip) = knot_hash_round(&mut input, input_size, input_lengths, 0, 0);
+    knot_hash_round(&mut input, input_size, input_lengths, &mut 0, &mut 0);
     input[0] * input[1]
 }
 
-fn knot_hash_round(input : &mut Vec<i32>, input_size: i32, input_lengths: &Vec<usize>, pos: usize, skip: usize) -> (usize, usize) {
-    let mut pos: usize = pos;
-    let mut skip: usize = skip;
+fn knot_hash_round(input : &mut Vec<i32>, input_size: i32, input_lengths: &Vec<usize>, pos: &mut usize, skip: &mut usize) {
 
     for length in input_lengths {
-        input.rev_circular_sub_slice(pos, *length);
-        pos = ((pos + length + skip) as i32 % input_size) as usize;
-        skip += 1;
+        input.rev_circular_sub_slice(*pos, *length);
+        *pos = ((*pos + length + *skip) as i32 % input_size) as usize;
+        *skip += 1;
     }
-    (pos, skip)
 }
 
 
