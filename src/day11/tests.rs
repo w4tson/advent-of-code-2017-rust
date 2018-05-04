0a06cbff47c42278d904a80a32ee48ab31377474
Paul Watson <paul.watson@pdwtech.com>
2018-05-04 20:30:26 +0100

    Day11 part2

--- src/day11/tests.rs
+++ src/day11/tests.rs
@@ -11,5 +11,28 @@ fn part1() {
         .fold(origin.clone(), |acc, direction| acc.next_hex(direction));
     
     assert_eq!(796, coord.distance_from(&origin));
-    
 }
+
+#[test]
+fn part2() {
+    let input = read_puzzle_input("aoc11");
+
+    let origin = HexCoord { x: 0, y: 0, z: 0 };
+
+    let max_distance : i32 = input.split(",")
+        .fold(vec![origin.clone()], compute_distances)
+        .iter()
+        .map(|coord| coord.distance_from(&origin))
+        .max()
+        .unwrap_or(0);
+    
+    assert_eq!(1585, max_distance);
+}
+
+fn compute_distances(acc: Vec<HexCoord>, direction: &str) -> Vec<HexCoord> {
+    let mut acc = acc;
+    let next = acc.last().unwrap().next_hex(direction);
+    acc.push(next);
+    acc
+}
+
