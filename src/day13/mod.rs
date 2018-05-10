a16d82a4139fae30ef553cca719c947da598dbf9
Paul Watson <paul.watson@pdwtech.com>
2018-05-10 07:36:07 +0100

    Day13 part2

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 #[cfg(test)]
 pub mod tests;
 
-fn calc_severity(firewall : HashMap<i32, i32>) -> i32 {
+fn calc_severity(firewall : &HashMap<i32, i32>) -> i32 {
     let width = firewall.keys().max().unwrap_or(&0);
     
     (0..=*width).fold(0, |acc, t| {
@@ -12,6 +12,17 @@ fn calc_severity(firewall : HashMap<i32, i32>) -> i32 {
             false => 0
         }
     })
+}
+
+fn calc_delay_to_avoid_getting_caught(firewall : &HashMap<i32, i32>) -> i32 {
+    let width = firewall.keys().max().unwrap_or(&0);
+
+    (0..).skip_while(|i| is_caught(&firewall, *width, *i)).next().unwrap_or(-1)
+}
+
+fn is_caught(firewall : &HashMap<i32, i32>, width: i32, delay: i32) -> bool {
+    (delay..=delay+width)
+        .any(|t| is_scan_at_row_zero(&firewall, t - delay, t))
 }
 
 fn is_scan_at_row_zero(firewall: &HashMap<i32, i32>, col: i32, pico_seconds: i32) -> bool {
--- src/day13/tests.rs
+++ src/day13/tests.rs
@@ -1,16 +1,23 @@
 use utils::file::read_puzzle_input;
 use std::collections::HashMap;
 use day13::calc_severity;
-use day13::is_scan_at_row_zero;
+use day13::calc_delay_to_avoid_getting_caught;
 
 #[test]
 fn part1() {
-    let firewall_config = read_puzzle_input("aoc13")
+    assert_eq!(788, calc_severity(&build_firewall()));
+}
+
+#[test]
+fn part2() {
+    assert_eq!(3905748, calc_delay_to_avoid_getting_caught(&build_firewall()));
+}
+
+fn build_firewall() -> HashMap<i32, i32> {
+    read_puzzle_input("aoc13")
         .lines()
         .map(to_entry)
-        .collect::<HashMap<i32, i32>>();    
-    
-    assert_eq!(788, calc_severity(firewall_config));
+        .collect::<HashMap<i32, i32>>()
 }
 
 fn to_entry(line: &str) -> (i32, i32) {
