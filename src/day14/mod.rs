6c03884523a92a84cca253f24642c51000a56d4f
Paul Watson <paul.watson@pdwtech.com>
2018-05-13 22:03:22 +0100

    Day14 part2 WIP

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -1,18 +1,50 @@
 use day10::knot_hash2;
+use std::vec::Vec;
 
 #[cfg(test)]
 pub mod tests;
 
-fn calc_used_sqaures(input: String) -> usize {
+fn calc_used_squares(input: String) -> usize {
     (0..128)
         .map(|i| knot_hash2(format!("{}-{}", input, i)))
         .map(|hash| row_count(&hash))
         .sum()
 }
 
-fn row_count(input : &String) -> usize {
-    let row: String = input.chars()
+fn calc_groups(input: &String) -> usize {
+    let grid : Vec<usize> = (0..128)
+        .flat_map(|row_index| {
+        knot_hash2(format!("{}-{}", input, row_index)).chars()
+            .enumerate()
+            .map(|(col_index, c)| match c {
+                '1' => row_index*128 + col_index,
+                _   => 0
+            }).collect::<Vec<usize>>()
+    
+    }).collect();
+    
+    println!("{:?}",  grid);
+    
+    0
+}
+
+
+
+fn to_row(row_string: &String) -> Vec<u32> {
+    row_string.chars()
+        .map(|c| match c {
+            '1' => 1,
+            _   => 0
+        })
+        .collect()
+}
+
+fn to_row_string(input: &String) -> String {
+    input.chars()
         .map(|c| format!("{:04b}", u32::from_str_radix(&c.to_string(), 16).expect("WTF")))
-        .collect();
-    row.chars().filter(|c| *c == '1').count()
+        .collect()
+}
+
+fn row_count(input : &String) -> usize {
+    to_row_string(&input).chars().filter(|c| *c == '1').count()
 }
--- src/day14/tests.rs
+++ src/day14/tests.rs
@@ -1,7 +1,12 @@
-use day14::calc_used_sqaures;
+use day14::calc_used_squares;
+use day14::calc_groups;
 
 #[test]
 fn part1() {
-    assert_eq!(8108, calc_used_sqaures("flqrgnkx".to_string()));
+    assert_eq!(8108, calc_used_squares("flqrgnkx".to_string()));
 }
 
+#[test]
+fn part2() {
+    calc_groups(&"flqrgnkx".to_string());  
+}
